1390f5c4f819981d3f8019a971133a9b84d67cda
Jason Peng <jasonpeng023@gmail.com>
2019-11-20 05:18:08 +0000

    Fix typo from adding new models

--- GraphNets/models/gcn_batch_reg.py
+++ GraphNets/models/gcn_batch_reg.py
@@ -9,7 +9,7 @@
 
 
 import torch
-from torch.nn import Linear, BatchNorm1D
+from torch.nn import Linear, BatchNorm1d
 import torch.nn.functional as F
 
 from torch_geometric.nn import GCNConv
@@ -19,21 +19,21 @@ class Net(torch.nn.Module):
     def __init__(self, w1=8, w2=32, w3=128):
         super(Net, self).__init__()
         self.conv1 = GCNConv(2, w1, cached=False)
-        self.batch1 = BatchNorm1D(w1)
+        self.bn1 = BatchNorm1d(w1)
         self.conv2 = GCNConv(w1, w2, cached=False)
-        self.batch2 = BatchNorm1D(w2)
+        self.bn2 = BatchNorm1d(w2)
         self.conv3 = GCNConv(w2, w3, cached=False)
-        self.batch3 = BatchNorm1D(w3)
+        self.bn3 = BatchNorm1d(w3)
         self.linear = Linear(w3, 3)
 
     def forward(self, batch):
         x, edge_index, batch_index = batch.x, batch.edge_index, batch.batch
         x = F.relu(self.conv1(x, edge_index))
-        x = self.batch1(x)
+        x = self.bn1(x)
         x = F.relu(self.conv2(x, edge_index))
-        x = self.batch2(x)
+        x = self.bn2(x)
         x = F.relu(self.conv3(x, edge_index))
-        x = self.batch3(x)
+        x = self.bn3(x)
         x = global_max_pool(x, batch_index)
         x = self.linear(x)
         return F.log_softmax(x, dim=1)
