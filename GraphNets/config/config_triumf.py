c9a414a060dc31926146c75b56baf50e487d7e60
Jason Peng <jasonpeng023@gmail.com>
2019-11-22 00:27:00 +0000

    New config for triumf

--- GraphNets/config/config_triumf.py
+++ GraphNets/config/config_triumf.py
@@ -1,15 +1,14 @@
-# for commit e54b672177b9a9afd8567bb57ce6 
+# for commit 886417eafdfba8aac1a1
 
 from config.easy_dict import EasyDict
 
 config = EasyDict()
 
 config.model_name = "gcn_kipf"
+config.model_kwargs = {}
 
-config.data_path = "/home/jpeng/test_data/IWCDmPMT_4pi_fulltank_test_graphnet.h5"
-config.train_indices_file = "/home/jpeng/test_data/IWCDmPMT_4pi_fulltank_test_splits/train.txt"
-config.val_indices_file = "/home/jpeng/test_data/IWCDmPMT_4pi_fulltank_test_splits/val.txt"
-config.test_indices_file = "/home/jpeng/test_data/IWCDmPMT_4pi_fulltank_test_splits/test.txt"
+config.data_path = "/data/WatChMaL/data/IWCDmPMT_4pi_fulltank_test/split_h5/IWCDmPMT_4pi_fulltank_test_graphnet_trainval.h5"
+config.indices_file = "/data/WatChMaL/data/IWCDmPMT_4pi_fulltank_test/split_h5/IWCDmPMT_4pi_fulltank_test_graphnet_trainval_idxs.txt"
 config.edge_index_pickle = "/home/jpeng/GraphNets/visualization/edges_dict.pkl"
 
 config.dump_path = "/home/jpeng/GraphNets/dump/gcn"
@@ -19,11 +18,14 @@ config.device = 'gpu'
 config.gpu_list = [0]
 
 config.batch_size = 64
-config.validate_batch_size = 64 
+config.validate_batch_size = 64
 config.lr=0.01
 config.weight_decay=5e-4
-
 config.epochs = 10
+
 config.report_interval = 50
 config.num_val_batches  = 32
 config.valid_interval   = 500
+
+config.validate_batch_size = 32
+config.validate_dump_interval = 256
