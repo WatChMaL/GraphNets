2405ef08d5cc2d153b9768c2a4d9773b104a8631
Jason Peng <jasonpeng023@gmail.com>
2019-12-01 23:18:12 -0800

    Update gcn_deeper

--- GraphNets/models/gcn_deeper.py
+++ GraphNets/models/gcn_deeper.py
@@ -14,13 +14,16 @@ from torch_geometric.nn import GCNConv
 from torch_geometric.nn import global_max_pool #, TopKPooling
 
 from custom_layers.topk_pool import TopKPooling
-#from custom_layers.remove_isolated_nodes import remove_isolated_nodes
+from custom_layers.gcn_conv import GCNConv as CustomGCN
 
 class Net(torch.nn.Module):
     def __init__(self, graph_ws=[128,128,128], lin_ws=[]):
         super(Net, self).__init__()
 
-        self.topk = TopKPooling(2, min_score=0.1)
+        self.conv = CustomGCN(2,1, cached=False)
+        self.conv.weight.requires_grad = False
+
+        self.topk = TopKPooling(1, min_score=0.1)
         self.topk.weight.requires_grad = False
 
         prev_w = 2
@@ -34,26 +37,19 @@ class Net(torch.nn.Module):
             prev_w = w
 
         self.lins = []
-        lin_ws.append(3) # This is match the output size
         for i, w in enumerate(lin_ws):
             setattr(self, "lin{}".format(i), Linear(prev_w, w))
             self.lins.append(getattr(self, "lin{}".format(i)))
             prev_w = w
 
+        self.lin_final = Linear(prev_w, 3)
+
 
     def forward(self, batch):
         x, edge_index, batch_index = batch.x, batch.edge_index, batch.batch
 
-        x, edge_index, _, batch_index, _, _ = self.topk(x, edge_index, None, batch_index)
-        #print(self.topk.weight)
-        #print("topk", batch_index.shape)
-
-        ## Removes isolated nodes, but could result in a graph completely disappearing.
-        ## Will not be using this
-        #edge_index, _, mask = remove_isolated_nodes(edge_index, num_nodes=batch_index.shape[0])
-        #x = x[mask]
-        #batch_index = batch_index[mask]
-        #print("remove isolate", batch_index.shape)
+        attn = self.conv(x, edge_index)
+        x, edge_index, _, batch_index, _, _ = self.topk(x, edge_index, None, batch_index, attn)
 
         for bn, conv in zip(self.bns, self.convs):
             x = F.relu(conv(x,edge_index))
@@ -61,7 +57,9 @@ class Net(torch.nn.Module):
 
         x = global_max_pool(x, batch_index)
         x = F.dropout(x, training=self.training)
-        for lin in self.lins:
-            x = lin(x)
 
+        for lin in self.lins:
+            x = F.relu(lin(x))
+
+        x = self.lin_final(x)
         return F.log_softmax(x, dim=1)
